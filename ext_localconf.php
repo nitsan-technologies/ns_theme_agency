9cce65f781a78704b1244e3c8c959ca23d9c59be
Sonal Chauhan <sonal.nitsan@gmail.com>
2020-06-08 17:07:46 +0530

    Added the compatibilty release with version 10

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,12 +8,4 @@ if (!defined('TYPO3_MODE')) {
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
 
 // Let's include PageTSconfig
-if (TYPO3_MODE === 'BE') {
-    call_user_func(
-        function ('ns_theme_agency') {
-            // Let's add default PageTSConfig for Backend layout, TCE form, Components etc.,
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_theme_agency/Configuration/PageTSconfig/setup.ts">');
-        },
-        $_EXTKEY
-    );
-}
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_theme_agency/Configuration/PageTSconfig/setup.ts">');
