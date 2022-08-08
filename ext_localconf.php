a73184b418ea805bb62a0ee26089257f1e506c03
nitsan <rmahesh.nitsan@gmail.com>
2022-08-08 15:39:15 +0530

    [TASK] Compatible with php version 8.1

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,5 @@
 <?php
-$_EXTKEY = "ns_theme_agency";
 
-// Let's configuration of this extension from "Extension Manager"
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
 
 // Let's include PageTSconfig
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:ns_theme_agency/Configuration/PageTSconfig/setup.typoscript'");
