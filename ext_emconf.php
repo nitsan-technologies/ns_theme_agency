7f8af515f6cef9771393e45a58e8ea1fd9f98106
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 12:37:30 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['ns_theme_agency'] = array(
 	'title' => '[NITSAN] T3 Agency TYPO3 Template',
-	'description' => 'Agency TYPO3 template is an ultimate tool to kickstart your project, either a software development company, or a product startup or any new business. Live-Demo: https://demo.t3terminal.com/?theme=t3t-agency PRO version: https://t3terminal.com/t3-agency-free-business-typo3-template',
+	'description' => 'Agency TYPO3 template is an ultimate tool to kickstart your project, either a software development company, or a product startup or any new business. Live-Demo: https://demo.t3planet.com/?theme=t3t-agency PRO version: https://t3planet.com/t3-agency-free-business-typo3-template',
 	'category' => 'templates',
 	'author' => 'Team NITSAN',
 	'author_email' => 'info@nitsan.in',
