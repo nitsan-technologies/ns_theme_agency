dba1eeba81aa33e2a6b8c67e0b90201495459252
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2023-05-26 19:06:20 +0530

    [TASK] Compatibility with TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,26 +1,38 @@
 <?php
 
-$EM_CONF['ns_theme_agency'] = array(
-	'title' => '[NITSAN] T3 Agency TYPO3 Template',
-	'description' => 'Agency TYPO3 template is an ultimate tool to kickstart your project, either a software development company, or a product startup or any new business. Live-Demo: https://demo.t3planet.com/?theme=t3t-agency PRO version: https://t3planet.com/t3-agency-free-business-typo3-template',
-	'category' => 'templates',
-	'author' => 'Team NITSAN',
-	'author_email' => 'info@nitsan.in',
-	'author_company' => 'NITSAN Technologies Pvt Ltd',
-	'state' => 'stable',
-	'version' => '4.0.5',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '10.0.0-11.5.99',
-			'ns_basetheme' => '10.0.0-11.5.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	//'autoload' => array(
-	//	'classmap' => array('Classes/'),
-	//),
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ns_theme_agency".
+ *
+ * Auto generated 11-05-2023 13:16
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => '[NITSAN] T3 Agency TYPO3 Template',
+  'description' => 'Agency TYPO3 template is an ultimate tool to kickstart your project, either a software development company, or a product startup or any new business. Live-Demo: https://demo.t3planet.com/?theme=t3t-agency PRO version: https://t3planet.com/t3-agency-free-business-typo3-template',
+  'category' => 'templates',
+  'version' => '4.0.5',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => false,
+  'author' => 'Team NITSAN',
+  'author_email' => 'info@nitsan.in',
+  'author_company' => 'NITSAN Technologies Pvt Ltd',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '12.0.0-12.5.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
 );
-?>
+
