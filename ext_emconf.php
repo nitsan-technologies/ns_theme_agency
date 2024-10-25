29118ff88ac7fcfbf2b29cfc48e68da793f1ecc9
NITSAN <sanjay@nitsan.in>
2024-10-25 13:42:14 +0530

    [RELEASE] Ready to release 12.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => '[NITSAN] T3 Agency TYPO3 Template',
   'description' => 'Agency TYPO3 template is the ultimate tool to kickstart your project, either a software development company or a product startup or any new business. Live-Demo: https://demo.t3planet.com/?theme=t3t-agency PRO version: https://t3planet.com/t3-agency-free-business-typo3-template',
   'category' => 'templates',
-  'version' => '12.0.0',
+  'version' => '12.0.1',
   'state' => 'stable',
   'uploadfolder' => false,
   'clearcacheonload' => false,
