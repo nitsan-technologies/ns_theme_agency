980d9b793753821adea4f702f5874bd89a71eba3
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2024-10-25 12:59:37 +0530

    [TASK] Resolved scroling issue in menu

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,20 +3,21 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "ns_theme_agency".
  *
- * Auto generated 11-05-2023 13:16
+ * Auto generated 25-10-2024 07:22
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF['ns_theme_agency'] = array (
+$EM_CONF[$_EXTKEY] = array (
   'title' => '[NITSAN] T3 Agency TYPO3 Template',
   'description' => 'Agency TYPO3 template is the ultimate tool to kickstart your project, either a software development company or a product startup or any new business. Live-Demo: https://demo.t3planet.com/?theme=t3t-agency PRO version: https://t3planet.com/t3-agency-free-business-typo3-template',
   'category' => 'templates',
-  'version' => '13.0.0',
+  'version' => '12.0.0',
   'state' => 'stable',
   'uploadfolder' => false,
+  'clearcacheonload' => false,
   'author' => 'Team NITSAN',
   'author_email' => 'info@nitsan.in',
   'author_company' => 'NITSAN Technologies Pvt Ltd',
@@ -24,7 +25,7 @@ $EM_CONF['ns_theme_agency'] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '13.0.0-13.4.99',
+      'typo3' => '12.0.0-12.5.99',
       'ns_basetheme' => '12.0.0-12.5.99',
     ),
     'conflicts' => 
