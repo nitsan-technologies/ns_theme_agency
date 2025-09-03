fcf489b07a780451171ffb3d26d95e3b168817f7
Saloni Thakkar <saloni.nitsan@gmail.com>
2025-09-03 13:40:51 +0530

    [TASK]content block

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,7 @@
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItemGroup(
+    'tt_content', // table
+    'CType', // typeField
+    't3themeagency_content_blocks', // group
+    'LLL:EXT:t3themeagency/Resources/Private/Language/locallang.xlf:contentBlocks.Block', // label
+    'before:default', // position
+);
